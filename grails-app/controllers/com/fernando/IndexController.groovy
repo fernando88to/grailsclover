a89d0fc423610541108d05da3304c8c07748196e
fernando henrique <fernando88to@gmail.com>
2023-11-30 10:34:50 -0300

    Initial commit

---
+++ grails-app/controllers/com/fernando/IndexController.groovy
@@ -0,0 +1,16 @@
+package com.fernando
+
+
+class IndexController {
+
+    def index() {
+        String nome = ''
+        [nome: nome]
+    }
+
+    def index2() {
+        String nome = ''
+        [nome: nome]
+    }
+
+}
